a39e6743ffd9aba0bc6ca93f32227b1fe76fb713
Thomas Gaudibert <tgaudibert@b2f.fr>
2020-01-18 14:55:23 +0100

    udpates

--- p2_Novastep/include/B_Connection.py
+++ p2_Novastep/include/B_Connection.py
@@ -1,37 +1,17 @@
 import bluetooth
 import time
 
-def receiveMessages():
-  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-  port = 1
-  server_sock.bind(("",port))
-  server_sock.listen(1)
+port = 1
+server_sock.bind(("",port))
+server_sock.listen(1)
 
-  client_sock,address = server_sock.accept()
-  print "Accepted connection from " + str(address)
+client_sock,address = server_sock.accept()
+print "Accepted connection from " + str(address)
 
-  data = client_sock.recv(1024)
-  print "received [%s]" % data
+data = client_sock.recv(1024)
+print "received [%s]" % data
 
-  client_sock.close()
-  server_sock.close()
-  return data
-
-
-def CalculForce(Force):
-    port = 1
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect(("B8:27:EB:65:1A:5C", port))
-    force = 0
-    while (force<Force):
-        print (force)
-        force+=1
-        time.sleep(1)
-    print (force)
-    print("Debut envoi")
-    sock.send("OK")
-    sock.close()
-
-Force=int(receiveMessages())
-CalculForce(Force)
+client_sock.close()
+server_sock.close()
