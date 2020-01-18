4a07352faed09f8636ca6ce8e3906b4fd45860aa
Thomas Gaudibert <tgaudibert@b2f.fr>
2020-01-18 18:02:42 +0100

    updates

--- p2_Novastep/include/B_client.py
+++ p2_Novastep/include/B_client.py
@@ -3,7 +3,7 @@ import time
 
 port = 10000
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-sock.connect(("B8:27:EB:65:1A:5C", port))
+sock.connect(('B8:27:EB:65:1A:5C', port))
 
 print("Debut envoi")
 sock.send("OK")
